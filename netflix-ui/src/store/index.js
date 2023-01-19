daea7101dd704ca241554691e507217590d5ee26
Abu Huraira <iamhuraira429@gmail.com>
2023-01-19 08:58:52 +0500

    Create Store

--- netflix-ui/src/store/index.js
+++ netflix-ui/src/store/index.js
@@ -1,1 +1,19 @@
-// Start Working Here on Netflix Clone
+import { configureStore, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    movies: [],
+    genresLoaded: false,
+    genres: [],
+};
+
+const NetlixSlice = createSlice({
+    name: 'netflix',
+    initialState,
+    extraReducers: (builder) => { },
+});
+
+export const store = configureStore({
+    reducer: {
+        netflix: NetlixSlice.reducer,
+    }
+});
