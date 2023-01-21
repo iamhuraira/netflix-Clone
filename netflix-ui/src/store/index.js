04bb7deb2222b4ff1d562393e3c23d703276523c
Abu Huraira <iamhuraira429@gmail.com>
2023-01-21 19:23:28 +0500

    full stop here.

--- netflix-ui/src/store/index.js
+++ netflix-ui/src/store/index.js
@@ -1,3 +1,4 @@
+import { async } from '@firebase/util';
 import { configureStore, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { Api_Key, TMBD_BASE_URL } from '../utils/constants';
@@ -8,9 +9,17 @@ const initialState = {
     genres: [],
 };
 export const getGenres = createAsyncThunk('netflix/genres', async () => {
-    const data = await axios.get(`${TMBD_BASE_URL}/genre/movie/list?api_key=${Api_Key}`);
-    console.log(data);
- });
+    const {data: {genres},} = await axios.get(`${TMBD_BASE_URL}/genre/movie/list?api_key=${Api_Key}`);
+    return genres;
+});
+const getRawData = (api, genres, paging) => {
+    const moviesArray = [];
+    for (let i = 1; moviesArray.length < 60 && i<10; i++) {
+ }
+export const fetchMovies = createAsyncThunk('netflix/trending', async ({ type }, thunkApi) => {
+    const { netflix: { genres }, } = thunkApi.getState();
+    return getRawData(`${TMBD_BASE_URL}/trending/${type}/week?api_key=${Api_Key}` , genres, true);
+ })
 
 const NetlixSlice = createSlice({
     name: 'netflix',
@@ -29,9 +38,4 @@ export const store = configureStore({
     }
 });
 
-// export const store = configureStore({
-//     reducer: {
-//         netflix: NetlixSlice.reducer,
-//     }
-// });
 
